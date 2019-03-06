a35835496de9205da5e3f1f59ee779b9d268b1e3
Rishabh Rajput <rishabh.rajput@tothenew.com>
2019-03-06 15:46:18 +0530

    gradle assignment added

---
+++ gradleDemo/src/main/java/com/gradle/First.java
@@ -0,0 +1,21 @@
+package com.gradle;
+
+
+import org.apache.commons.lang3.StringUtils;
+
+public class First {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World !!");
+
+        String s = "  For StringUtil ";
+
+        System.out.println(StringUtils.substring(s,6));
+
+
+
+
+    }
+
+}
---
+++ gradleDemo/src/main/java2/FirstJava2.java
@@ -0,0 +1,6 @@
+public class FirstJava2 {
+
+    public static void main(String[] args) {
+        System.out.println("hello world form java2");
+    }
+}
